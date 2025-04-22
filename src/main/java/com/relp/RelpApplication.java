92e21a2256fde7011956949fcfae39b944a79a38
sunnyfernandes565@gmail.com <sunnyfernandes565@gmail.com>
2025-04-22 19:43:20 +0530

    fist commit

---
+++ src/main/java/com/relp/RelpApplication.java
@@ -0,0 +1,13 @@
+package com.relp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/relp/RelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
